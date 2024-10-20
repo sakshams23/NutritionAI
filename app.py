d5ce58a25a5eb6cff8f671b197ed3e8492b33c56
Saksham Soni <sonisaksham20208@gmail.com>
2024-10-20 22:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def input_image_setup(uploaded_file):
 st.set_page_config(page_title="Gemini Image Demo")
 
 # App UI
-st.header("Gemini Application")
+st.header("Food & NutritionAI")
 input = st.text_input("Input Prompt: ", key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image = ""   
